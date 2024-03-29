c0f49f3f2feb8e4330dec0735390d61d39cc1ea8
Air <160431936+AirDead@users.noreply.github.com>
2024-03-29 21:56:49 +0200

    Update Listener.kt

--- src/main/java/me/airdead/ru/utils/server/Listener.kt
+++ src/main/java/me/airdead/ru/utils/server/Listener.kt
@@ -3,7 +3,8 @@ package me.airdead.ru.utils.server
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
-class Listener(var plugin: JavaPlugin) : Listener {
+open class Listener(protected val plugin: JavaPlugin) : Listener {
+
     init {
         plugin.server.pluginManager.registerEvents(this, plugin)
     }
