0c7ea40732e813883a1300cd8e5613ee53fb7ad7
Air <160431936+AirDead@users.noreply.github.com>
2024-03-29 13:37:08 +0200

    Add files via upload

---
+++ src/main/java/me/airdead/ru/Plugin.kt
@@ -0,0 +1,13 @@
+package me.airdead.ru
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Plugin : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
